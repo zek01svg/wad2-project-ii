1ecb9d434db4dcdd6d78cd78c854df451457458d
zek01svg <cia.velasco.2024@smu.edu.sg>
2025-10-15 14:17:08 +0800

    minor modifications to package.json and server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,45 +1,40 @@
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 import { env } from "#server/env.ts";
 import { auth } from "#server/lib/auth.ts";
 
-const app = new Hono()
+const baseRoutes = new Hono()
   .get("/health", (c) => {
     return c.json({
       status: "ok",
     });
   })
-  .use(
-    "/*",
-    serveStatic({
-      root: "./dist/static",
-      index: "index.html",
-      onNotFound: (path) => {
-        console.log(path);
-      },
-    }),
-  );
-
-app
-  .basePath("/api")
-  .get("/runtime.js", (c) => {
+  .get("/api/runtime.js", (c) => {
     return c.text(
       `
-      window.__env = ${JSON.stringify(Object.fromEntries(Object.entries(env).filter(([key]) => key.startsWith("VITE_"))), null, 2)}
-      `.trim(),
+    window.__env = ${JSON.stringify(Object.fromEntries(Object.entries(env).filter(([key]) => key.startsWith("VITE_"))), null, 2)}
+    `.trim(),
       200,
       { "Content-Type": "application/javascript" },
     );
   })
-  .on(["POST", "GET"], "/auth/*", (c) => {
+  .on(["POST", "GET"], "/api/auth/*", (c) => {
     return auth.handler(c.req.raw);
-  });
+  })
+  .use("/assets/*", serveStatic({ root: "./dist/static" }))
+  .use("/*", serveStatic({ root: "./dist/static" }))
+  .get("*", serveStatic({ path: "./dist/static/index.html" }));
 
-serve({
+const apiRoutes = new Hono();
+
+const app = new Hono().route("/api", apiRoutes).route("/", baseRoutes);
+
+const server = {
   port: 4001,
   fetch: app.fetch,
-});
+};
 
 console.log(`App is running on port 4001`);
+
+export default server;
